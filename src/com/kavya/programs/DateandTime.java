d79edc6a4f8bd5fcd903829c02444dec46ed2323
Kavya12mk <kavyamadan1221@gmail.com>
2024-06-26 11:05:22 +0530

    *

---
+++ src/com/kavya/programs/DateandTime.java
@@ -0,0 +1,9 @@
+package com.kavya.programs;
+
+public class DateandTime
+{
+    public static void main(String[] args)
+    {
+        System.out.println(System.currentTimeMillis()/1000/3600/24/365);
+    }
+}
